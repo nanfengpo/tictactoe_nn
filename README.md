5cff3dce83eb6d5c304fe3eae8c5c9c5a46bd6d8
Yaroslav Protsenko <yprotsenko@4tifier.com>
2017-04-26 13:43:35 +0300

    improved readme, added docs for configuration

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 The AI is implemented using Convolutional NN and is trained with 
 the [Deep Q-Learning algorithm](https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf).
+
+## How to use
+
+1. Add your own local configuration file at `playground/_train_config.py`
+2. Extend the `tictactoe_nn.config.TrainConfig` class and define options you need.
+See the `TrainConfig` for available options.
+3. I recommend you to install the `fflib` - apart of other things it provides the tool
+for quickly running scripts from the `playground` directory.  
+`pip install https://github.com/faddey-w/fflib/archive/master.zip`
+4. Run the training script. If you've installed the fflib, simply type `play run`, 
+otherwise - `python3 -m playground.run`
--- tictactoe_nn/config.py
+++ tictactoe_nn/config.py
@@ -1,3 +1,4 @@
+import pathlib
 from collections import namedtuple
 from .utils import UndefinedAttr as _Undefined
 
@@ -6,22 +7,41 @@ OpponentDef = namedtuple('OpponentDef', 'layers_def variables_scope save_path')
 
 
 class TrainConfig:
+    """Default configuration for training"""
 
+    """A random seed to use for variables initialization"""
     seed = None
 
-    variables_scope = _Undefined()
-    save_path = _Undefined()
+    """Scope for model's variables"""
+    variables_scope = _Undefined()  # type: str
+
+    """Where to save the trained model"""
+    save_path = _Undefined()  # type: str
+
+    """A list of tictactoe_nn.layers.Layer objects.
+    Defines the structure of the network."""
     layers_def = _Undefined()  # type: list
 
+    """The size of game field. Should be tuple of two integers"""
     field_size = (3, 3)
+    """Minimum strike length needed to win the game"""
     win_length = 3
 
+    """Number of games per training batch."""
     n_games = 1
 
+    """
+    List of OpponentDef objects.
+    Use it if you want to train your model to play against other trained models
+    """
     opponents = []
 
-    models_root = '.models'
+    """
+    The root of all saved models. Defaults to <repo-root>/.models
+    """
+    models_root = str(pathlib.Path(__file__).parents[1].joinpath('.models'))
 
     learning_rate = 0.01
 
+    """Maximum training iterations."""
     n_iters = 10000
